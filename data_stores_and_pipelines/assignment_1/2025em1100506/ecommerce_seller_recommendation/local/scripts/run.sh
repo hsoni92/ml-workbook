c41e24611ae49860ea2e0fc3fdf7aba3bdc173c3
hsoni92 <h.soni1321992@gmail.com>
2025-11-19 23:49:26 +0530

    himanshu.soni-19-11-2025-23:49:26

---
+++ data_stores_and_pipelines/assignment_1/2025em1100506/ecommerce_seller_recommendation/local/scripts/run.sh
@@ -0,0 +1,5 @@
+./scripts/etl_seller_catalog_spark_submit.sh
+./scripts/etl_company_sales_spark_submit.sh
+./scripts/etl_competitor_sales_spark_submit.sh
+./scripts/consumption_recommendation_spark_submit.sh
+./scripts/visualize_recommendations.sh
