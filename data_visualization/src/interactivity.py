7d449828d45d4642f77e5f6fdd3c107bcb240d28
hsoni92 <h.soni1321992@gmail.com>
2025-12-01 05:01:58 +0530

    himanshu.soni-01-12-2025-05:01:58

--- data_visualization/src/interactivity.py
+++ data_visualization/src/interactivity.py
@@ -2,21 +2,21 @@
 Interactivity module for dashboard widgets and callbacks.
 """
 
-from bokeh.models import Select, MultiSelect, CheckboxGroup, CustomJS
+from bokeh.models import Select, MultiSelect, CheckboxGroup, CustomJS, Button
 from bokeh.models import ColumnDataSource
 import pandas as pd
 
 
 def create_constituency_selector(constituencies, default_selection=None):
     """
-    Create a multi-select widget for constituency selection.
+    Create a multi-select widget for constituency selection with a Select All button.
 
     Args:
         constituencies: List of constituency names
         default_selection: List of default selected constituencies
 
     Returns:
-        MultiSelect widget
+        tuple: (MultiSelect widget, Select All Button)
     """
     if default_selection is None:
         default_selection = constituencies
@@ -29,7 +29,24 @@ def create_constituency_selector(constituencies, default_selection=None):
         height=250
     )
 
-    return widget
+    # Create Select All button
+    select_all_button = Button(
+        label="Select All",
+        button_type="default",
+        width=200,
+        height=30
+    )
+
+    # Add JavaScript callback to select all constituencies
+    select_all_button.js_on_click(CustomJS(
+        args={'multiselect': widget, 'all_options': constituencies},
+        code="""
+        multiselect.value = all_options;
+        multiselect.change.emit();
+        """
+    ))
+
+    return widget, select_all_button
 
 
 def create_year_filter(years, default_selection=None):
